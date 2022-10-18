3c654114ee2082b5f490602f256a16e883cdd1f7
Oleksandr Pikush <pikush.oleksandr@gmail.com>
2022-10-18 15:12:58 +0300

    2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = ingredients.map(ingredient => {
+  const li = document.createElement('li');
+  li.textContent = ingredient;
+  li.classList = 'item';
+  return li;
+})
+ 
+console.log(list);
+
+const ulRef = document.querySelector('#ingredients');
+ulRef.append(...list);
