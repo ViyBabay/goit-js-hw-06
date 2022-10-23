3067191b63f32a3c4bc144f4b9997b52519a3b44
Oleksandr Pikush <pikush.oleksandr@gmail.com>
2022-10-23 22:43:10 +0300

    +5

--- js/task-05.js
+++ js/task-05.js
@@ -1,11 +1,21 @@
-const refs = {
-    input: document.querySelector('#name-input'),
-    nameLabel: document.querySelector('#name-output'),
-}
+// const refs = {
+//     input: document.querySelector('#name-input'),
+//     nameLabel: document.querySelector('#name-output'),
+// }
 
-refs.input.addEventListener('input', onInputChange);
+// refs.input.addEventListener('input', onInputChange);
 
-function onInputChange(event) {
-    refs.nameLabel.textContent = event.currentTarget.value
+// function onInputChange(event) {
+//     refs.nameLabel.textContent = event.currentTarget.value
+// }
 
+const input = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+input.addEventListener("input", onInputEntryData);
+function onInputEntryData(evt) {
+  output.textContent = evt.currentTarget.value;
+    if (evt.target.value === "") {
+      output.textContent = "Anonymous";
+    }
 }
--- js/task-09.js
+++ js/task-09.js
@@ -2,11 +2,13 @@ const body = document.querySelector("body");
 const colorVal = document.querySelector("span.color");
 const btn = document.querySelector("button.change-color");
 
+
 btn.addEventListener("click", (evt) => {
   let color = getRandomHexColor();
   body.style.backgroundColor = color;
   colorVal.textContent = color;
 });
+
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
